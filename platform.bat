d4729f4dc8a14a6e899d86c6ae668ccc7cbf8fdb
GOOGLE\slhong <slhong@google.com>
2017-10-17 21:41:38 -0600

    'ja'
    Change-Id: Icf983befa8179578815e704274eb0b64d36c7bac

--- platform.bat
+++ platform.bat
@@ -4,18 +4,15 @@ net stop "GAgent Monitor"
 reg delete HKLM\SOFTWARE\Google\GAgent /f
 mkdir %SystemDrive%\\AppBridge.SAAS.Platform
 mkdir %SystemDrive%\\AppBridge.Management.Windows.UI
-call gsutil cp gs://appbridge-build-artifacts/prod/appbridge/appbridge/release_back_end/12/20171005-152557/artifacts/AppBridge.Saas.Platform.zip %SystemDrive%\\AppBridge.SAAS.Platform.zip
-call gsutil cp gs://appbridge-build-artifacts/prod/appbridge/appbridge/release_back_end/12/20171005-152557/artifacts/AppBridge.Saas.Platform.zip %SystemDrive%\\AppBridge.Management.Windows.UI.zip
-powershell.exe Expand-Archive -Path %SystemDrive%\\AppBridge.SAAS.Platform.zip -DestinationPath %SystemDrive%\\AppBridge.SAAS.Platform
-powershell.exe Expand-Archive -Path %SystemDrive%\\AppBridge.Management.Windows.UI.zip -DestinationPath %SystemDrive%\\AppBridge.Management.Windows.UI
+call gsutil cp gs://appbridge-build-artifacts/prod/appbridge/appbridge/release_back_end/12/20171005-152557/artifacts/AppBridge_Migration_Platform_Installer_2_0_0_20.exe %SystemDrive%\\AppBridge_Migration_Platform_Installer.exe
 REM Enable high performance mode
 powercfg -setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
-pushd %SystemDrive%\AppBridge.SAAS.Platform
-AppBridge.Saas.Platform.exe --install
+pushd %SystemDrive%
+AppBridge_Migration_Platform_Installer.exe --install /SILENT
 popd
 REM change the delayed automatic start to 1 sec instead of the defaulted 120 sec wait
 REG ADD HKLM\SYSTEM\CurrentControlSet\Services\AppBridge.Local.ServiceHost\AutoStartDelay /t REG_DWORD /d 1 /f
 REM Configure firewall. Note service="any" is important. If service is set only to appbridge service host, the firewall will block AppBridge service.
-netsh.exe advfirewall firewall add rule name="AB Node Platform 80" dir="in" protocol="TCP" localport=80 action="allow" service="any" enable="yes"
+netsh.exe advfirewall firewall add rule name="AB Node Platform 5131" dir="in" protocol="TCP" localport=5131 action="allow" service="any" enable="yes"
 netsh.exe advfirewall firewall add rule name="AB Node Platform 443" dir="in" protocol="TCP" localport=443 action="allow" service="any" enable="yes"
 shutdown /r /t 0 /f /d p:0:0
