15f8b792684ae031f8e11cf9344f4a3f679c1b0c
mikyxdxd <mikyxdxd@gmail.com>
2017-10-18 16:33:21 -0600

    Update platform.bat

--- platform.bat
+++ platform.bat
@@ -6,18 +6,16 @@ call gsutil cp gs://appbridge-build-artifacts/prod/appbridge/appbridge/release_b
 REM Enable high performance mode
 powercfg -setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 pushd %SystemDrive%
-mkdir %SystemDrive%\\Installing
 %SystemDrive%\\AppBridge_Migration_Platform_Installer.exe --install /VERYSILENT /norestart
-mkdir %SystemDrive%\\Installed
 popd
 
-mkdir %SystemDrive%\\autostart
+
 sc config "AppBridge.Local.ServiceHost" start=auto
 sc start "AppBridge.Local.ServiceHost"
-mkdir %SystemDrive%\\autostarted
+
 
 netsh.exe advfirewall firewall add rule name="AB Node Platform 5131" dir="in" protocol="TCP" localport=5131 action="allow" service="any" enable="yes"
 netsh.exe advfirewall firewall add rule name="AB Node Platform 443" dir="in" protocol="TCP" localport=443 action="allow" service="any" enable="yes"
 netsh.exe advfirewall firewall add rule name="AB Node Platform 80" dir="in" protocol="TCP" localport=80 action="allow" service="any" enable="yes"
   
-mkdir %SystemDrive%\\firewallcomplete
+
