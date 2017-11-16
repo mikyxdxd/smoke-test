96360661a8b2c538a4493edec7426c8686400eb0
mikyxdxd <mikyxdxd@gmail.com>
2017-11-15 18:21:36 -0700

    Update platform.bat

--- platform.bat
+++ platform.bat
@@ -2,7 +2,7 @@ call %SystemDrive%\\GAgent_3.1.2.6_amd64.exe
 net stop "GAgent Monitor"
 reg delete HKLM\SOFTWARE\Google\GAgent /f
 mkdir %SystemDrive%\\Starting
-call gsutil cp gs://appbridge-build-artifacts/prod/appbridge/appbridge/release_back_end/12/20171005-152557/artifacts/AppBridge_Migration_Platform_Installer_2_0_0_20.exe %SystemDrive%\\AppBridge_Migration_Platform_Installer.exe
+call gsutil cp gs://appbridge-build-artifacts/prod/appbridge/appbridge/release_back_end/17/20171103-083154/artifacts/AppBridge_Migration_Platform_Installer_2_0_0_22.exe %SystemDrive%\\AppBridge_Migration_Platform_Installer.exe
 REM Enable high performance mode
 powercfg -setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
 pushd %SystemDrive%
